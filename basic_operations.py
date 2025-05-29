eb1ba3e7b3baebd121e398d2c344815deca7ea7c
Alakiri Counsellor <counsellor829@gmail.com>
2025-05-29 18:17:57 +0100

    concluded basic operations

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+sumTotal=number1 + number2
+print("Addition of " + str(number1)+ " and "+ str(number2)+ " is "+ str(sumTotal))
+substraction= number1 - number2
+print("substraction of " + str(number1)+ " and "+ str(number2)+ " is "+ str(substraction))
+multiplication= number1 * number2
+print("multiplication of " +str(number1)+ " and "+ str(number2)+ " is "+ str(multiplication))
