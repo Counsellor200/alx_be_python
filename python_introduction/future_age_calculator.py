828c6b6205b72938b71b70d5563b121583651363
Alakiri Counsellor <counsellor829@gmail.com>
2025-05-31 17:48:48 +0100

    files moved to directory

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -2,4 +2,4 @@ current_age = 30
 current_year = 2023
 future_year = 2050
 future_age =  current_age + (future_year - current_year)
-print (f"In {future_year}, you will be {future_age} years old." )
+print (f"In {future_year}, you will be {future_age} years old." ) 
